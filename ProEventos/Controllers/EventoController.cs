bdab6a813f0046719817c85b1e75b59a38c028cf
Evandro-Prog <evandro.presenci@gmail.com>
2021-10-16 09:21:31 -0300

    Projeto Pro Eventos
    - Inicio do projeto;
    - Desenvolvimento primeiro Controller
    - Desenvolvimento primeiro Model;
    - Testes no Postman;

---
+++ ProEventos/Controllers/EventoController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProEventos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[]
+        {
+            new Evento()
+            {
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "São Paulo",
+                Lote = "1º",
+                QtdPessoas = 250,
+                DataEvanto = DateTime.Now.AddDays(2).ToString(),
+                ImgUrl = "Foto.png"
+            },
+            new Evento()
+            {
+                EventoId = 2,
+                Tema = "Angular 11 e .NET 5",
+                Local = "Rio de Janeiro",
+                Lote = "1º",
+                QtdPessoas = 250,
+                DataEvanto = DateTime.Now.AddDays(2).ToString(),
+                ImgUrl = "Foto.png"
+            }
+        };   
+    public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;                 
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"value = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"value = {id}";
+        }
+    }
+}
---
+++ ProEventos/Models/Evento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public string DataEvanto { get; set; }
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string Lote { get; set; }
+        public string ImgUrl { get; set; }        
+    }
+}
