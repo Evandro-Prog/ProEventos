0f055c1d2acd7bfe59b37077b3b0589849bc9e93
Evandro-Prog <evandro.presenci@gmail.com>
2021-10-16 10:57:46 -0300

    Update EventoController.cs

--- ProEventos/Controllers/EventoController.cs
+++ ProEventos/Controllers/EventoController.cs
@@ -11,44 +11,23 @@ namespace ProEventos.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-
-        public IEnumerable<Evento> _evento = new Evento[]
+        private readonly ProEventosContext _context;        
+            
+    public EventoController(ProEventosContext context)
         {
-            new Evento()
-            {
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "São Paulo",
-                Lote = "1º",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImgUrl = "Foto.png"
-            },
-            new Evento()
-            {
-                EventoId = 2,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Rio de Janeiro",
-                Lote = "1º",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImgUrl = "Foto.png"
-            }
-        };   
-    public EventoController()
-        {
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;                 
+            return _context.Eventos;                 
         }
 
         [HttpGet("{id}")]
         public IEnumerable<Evento> GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.Where(evento => evento.EventoId == id);
         }
 
         [HttpPost]
